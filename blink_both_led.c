d0b6a76d5ed62d26f5c50287aa975a31862e232f
priyapappachan <priyapappachan010@gmail.com>
2011-12-14 13:30:40 +0530

    blinks both leds

---
+++ blink_both_led.c
@@ -0,0 +1,20 @@
+#include<msp430.h>
+
+void delay(void);
+
+main()
+{
+	P1DIR = 0x41;
+	P1OUT = 0x41;
+	while(1)
+	{
+		delay();	
+		P1OUT^= 0x41;
+	}
+}
+
+void delay()
+{
+	int i;
+	for (i=0;i<10000;i++);
+}
